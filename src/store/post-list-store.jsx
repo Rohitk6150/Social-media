0ef4a343b5c7bce22055c54e080071b71558c4a4
rohitk6150 <115240491+Rohitk6150@users.noreply.github.com>
2024-06-29 07:49:42 +0530

    Update post-list-store.jsx

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -22,7 +22,7 @@ const postListReducer = (currPostList, action) => {
 };
 
 const PostListProvider = ({children}) =>{
-    const[postList, dispatchPostList] = useReducer(postListReducer,DEFAULT_POST_LIST);
+    const[postList, dispatchPostList] = useReducer(postListReducer,[]);
 
     const addPost = (userId,postTitle,postBody, reactions, tags) =>{
         dispatchPostList({
@@ -63,25 +63,6 @@ const PostListProvider = ({children}) =>{
     </PostList.Provider>
     );
 };
-
-const DEFAULT_POST_LIST = [
-    {
-        id:"1",
-        title: "Going to Mumbai",
-        body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
-        reactions: 2,
-        userId: "user-9",
-        tags:["vacation", "Mumbai", "Enjoying"],
-    },
-    {
-        id:"2",
-        title: "Pass ho bhai",
-        body: "4 saal ki masti k baad bhi ho gaye hain pass. Hard to believe.",
-        reactions: 15,
-        userId: "user-12",
-        tags:["Graduating", "Unbelievable"],
-    },
-];
 
 export default PostListProvider;
 
